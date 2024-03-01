4c9acfe357335181875f26ff8053c00da189577c
SCMkhwanazi <160224688+SCMkhwanazi@users.noreply.github.com>
2024-03-01 04:53:17 +0000

    Added flack,Added dynamic data

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask,render_template
+
+
+app = Flask(__name__)
+LANGUAGES = [
+  {
+  'id': 1,
+  'title': 'English',
+},
+  {
+  'id': 2,
+  'title': 'Sepedi',
+  },
+  {
+  'id': 3,
+  'title': 'Zulu',
+  },
+]
+@app.route('/')
+def hello_world():
+  return render_template('home.html',languages = LANGUAGES)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0',debug=True)
